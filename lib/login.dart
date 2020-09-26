0c57cd199829be38a4a97351aa556ca836201b34
Amey Sunu <ameysunu00719@gmail.com>
2020-09-26 14:17:53 +0300

    google sign in added

--- lib/login.dart
+++ lib/login.dart
@@ -5,6 +5,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+String name;
+String email;
+String imageUrl;
+
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
@@ -25,6 +29,13 @@ Future<String> signInWithGoogle() async {
   final User user = authResult.user;
 
   if (user != null) {
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(user.photoURL != null);
+    name = user.displayName;
+    email = user.email;
+    imageUrl = user.photoURL;
+
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);
 
