2caf51788bc777faf219567e8501b919daa5d7f1
Amey Sunu <ameysunu00719@gmail.com>
2020-09-27 13:23:12 +0300

    user added

--- lib/user.dart
+++ lib/user.dart
@@ -14,53 +14,125 @@ class _UserState extends State<User> {
       backgroundColor: Hexcolor('#845EC2'),
       body: SafeArea(
         child: SingleChildScrollView(
-            child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Card(
-                color: Hexcolor('#D5CCE6'),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Hexcolor('#D5CCE6'),
-                        backgroundImage: NetworkImage(imageUrl),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Hexcolor('#D5CCE6'),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Hexcolor('#D5CCE6'),
+                          backgroundImage: NetworkImage(imageUrl),
+                        ),
                       ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            name,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Poppins',
-                                fontSize: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              name,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Poppins',
+                                  fontSize: 20),
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            email,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Poppins',
-                                fontSize: 14),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              email,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Poppins',
+                                  fontSize: 14),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        )),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "My Friends",
+                  style: TextStyle(
+                      fontFamily: 'Poppins', color: Colors.white, fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 200,
+                        width: 200,
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Container(
+                                  height: 150,
+                                  width: 150,
+                                  child: Image.network(
+                                      "https://www.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_client_male_person_user_work_sport_beard_team_glasses-512.png")),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  "Mikey",
+                                  style: TextStyle(fontFamily: 'Poppins'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 200,
+                        width: 200,
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Container(
+                                  height: 150,
+                                  width: 150,
+                                  child: Image.network(
+                                      "https://www.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png")),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  "Anna",
+                                  style: TextStyle(fontFamily: 'Poppins'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "My Friend Requests",
+                  style: TextStyle(
+                      fontFamily: 'Poppins', color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
