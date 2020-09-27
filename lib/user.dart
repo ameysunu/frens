f5246abe3ae433c270cb8e42a3042aa5fe118220
Amey Sunu <ameysunu00719@gmail.com>
2020-09-27 13:30:24 +0300

    user added

--- lib/user.dart
+++ lib/user.dart
@@ -130,6 +130,75 @@ class _UserState extends State<User> {
                       fontFamily: 'Poppins', color: Colors.white, fontSize: 20),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Hexcolor('#FF9671'),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          height: 100,
+                          width: 100,
+                          child: CircleAvatar(
+                            backgroundColor: Hexcolor('#FF9671'),
+                            child: Image.network(
+                                'http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png'),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "David Jonah",
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          ),
+                          Text(
+                            "davidfreek@example.com",
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Hexcolor('#FFC75F'),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          height: 100,
+                          width: 100,
+                          child: CircleAvatar(
+                            child: Image.network(
+                                'https://cms-assets.tutsplus.com/uploads/users/107/posts/26101/image/22-x-men-avatars.jpg'),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Chris Martin",
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          ),
+                          Text(
+                            "chrismarty@example.com",
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
